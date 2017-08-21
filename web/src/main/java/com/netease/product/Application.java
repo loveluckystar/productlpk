5790c22bedab258bb46cb777c69eb2c938de414f
xingyu.mxy <xingyu.mxy@alibaba-inc.com>
2017-08-21 16:02:37 +0800

    提交项目结构

---
+++ web/src/main/java/com/netease/product/Application.java
@@ -0,0 +1,14 @@
+package com.netease.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mxy on 2017/8/20.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
